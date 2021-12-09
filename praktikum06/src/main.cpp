a90c4f8ff27e9f0fdc49f67b9b634c8d33073059
Julain Voss <julian.voss97@outlook.de>
2021-12-09 13:42:26 +0100

    Main read file

--- praktikum06/src/main.cpp
+++ praktikum06/src/main.cpp
@@ -4,12 +4,32 @@
 
 
 #include <iostream>
+#include <fstream>
+#include <cstring>
+
+using std::ifstream;
 
 int main (int argc, char **argv) {
+    std::string url;
+    const char* urls[20];
+    if(argv[1] != nullptr) {
+        ifstream input_file(argv[1]);
 
-    for (int i = 0; i < argc; i++) {
-        std::cout<< "Argument " << i << ": " << argv[i] << std::endl;
+        if(input_file.is_open()) {
+            unsigned int i = 0;
+            while(std::getline(input_file, url)) {
+                urls[i] = strdup(url.c_str());
+                printf("%s\n", urls[i]);
+                i++;
+            }
+        }else {
+            std::cerr << "Datei " << argv[1] << " konnte nicht geöffnet werden." << std::endl;
+        }
+        input_file.close();
+    }else {
+        std::cerr << "Bitte Steuerdatei angeben." << std::endl;
     }
+
 
     return 0;
 }
