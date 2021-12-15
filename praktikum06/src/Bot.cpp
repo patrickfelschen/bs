623f5c382e9e3e7be9bcda385eb6f22d5d25e3f2
patrickfelschen <patrick@felschen.de>
2021-12-15 12:54:48 +0100

    regex

--- praktikum06/src/Bot.cpp
+++ praktikum06/src/Bot.cpp
@@ -4,32 +4,41 @@
 
 #include "Bot.h"
 
-#define RED   "\x1B[31m"
-#define GRN   "\x1B[32m"
-#define YEL   "\x1B[33m"
+#define RED "\x1B[31m"
+#define GRN "\x1B[32m"
+#define YEL "\x1B[33m"
+#define URLPATTERN "((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)"
+
+#include <regex>
 
 Bot::Bot() {}
 
 Bot::~Bot() {}
 
 /* Producer */
-void *producer(void *q, char* fileName) {
+void *producer(void *q, char *fileName) {
     Queue *queue = (Queue *) q;
-    //// Urls aus Datei einlesen
+    const std::regex pattern(URLPATTERN);
     std::ifstream file(fileName);
     std::string str;
     while (std::getline(file, str)) {
-        std::unique_lock<std::mutex> lock(queue->mutex);
-        while(queue->isFull()){
-            queue->notFull.wait(lock);
-        }
         char *cstr = new char[str.length() + 1];
         strcpy(cstr, str.c_str());
+        if (!std::regex_match(cstr, pattern)) {
+            printf(GRN"producer (0): wrong url: %s\n", cstr);
+            continue;
+        }
+        std::unique_lock<std::mutex> lock(queue->mutex);
+        while (queue->isFull()) {
+            printf(GRN"producer (0): isFull\n");
+            queue->notFull.wait(lock);
+        }
         queue->addItem(cstr);
-        printf(GRN"producer: %s\n", cstr);
+        printf(GRN"producer (0): produced %s\n", cstr);
         queue->notEmpty.notify_all();
     }
     queue->setEnd(true);
+    file.close();
     return nullptr;
 }
 
@@ -60,18 +69,17 @@ void do_consume(char *url, int id) {
 /* Consumer */
 void *consumer(void *q, int id) {
     Queue *queue = (Queue *) q;
-    while(true) {
+    while (true) {
         std::unique_lock<std::mutex> lock(queue->mutex);
-        while(queue->isEmpty()){
-            //// Queue erreicht das Ende und ist leer
-            if(queue->isEnd()) {
-                printf(YEL"consumer (%i): isEnd\n", id);
+        while (queue->isEmpty()) {
+            if (queue->isEnd()) {
+                printf(RED"consumer (%i): isEnd\n", id);
                 return nullptr;
             }
-            printf(YEL"consumer (%i): isEmpty\n", id);
+            printf(RED"consumer (%i): isEmpty\n", id);
             queue->notEmpty.wait(lock);
         }
-        char* url;
+        char *url;
         queue->delItem(&url);
         lock.unlock();
         do_consume(url, id);
@@ -79,24 +87,20 @@ void *consumer(void *q, int id) {
     }
 }
 
-void Bot::start(char* fileName, int queueSize, int threadCount) {
-    if(queueSize <= 0 || threadCount <= 0){
+void Bot::start(char *fileName, int queueSize, int threadCount) {
+    if (queueSize <= 0 || threadCount <= 0) {
         exit(-1);
     }
-
-    std::thread* threads = new std::thread[threadCount];
+    std::thread *threads = new std::thread[threadCount];
     Queue queue(queueSize);
-
 #ifdef __linux__
     webreq_set_output_path("./output");
 #endif
     //// Reader Thread
     std::thread pro(producer, &queue, fileName);
-
     struct timeval tv;
     gettimeofday(&tv, nullptr);
     double startTime = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
-
     //// Client Threads
     for (int i = 0; i < threadCount; i++) {
         threads[i] = std::thread(consumer, &queue, i);
@@ -105,11 +109,9 @@ void Bot::start(char* fileName, int queueSize, int threadCount) {
     for (int i = 0; i < threadCount; i++) {
         threads[i].join();
     }
-
     gettimeofday(&tv, nullptr);
     double endTime = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
-    printf("Dauer: %d\n", (int)(endTime - startTime));
-
+    printf(YEL"Dauer: %d\n", (int) (endTime - startTime));
     delete[] threads;
 #ifdef __linux__
     webreq_cleanup();
