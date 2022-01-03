66af76b62b1d30356e6a62ec73e2fb852e46af47
patrickfelschen <patrick@felschen.de>
2022-01-03 17:41:13 +0100

    rechte

--- praktikum07/src/ListDir.cpp
+++ praktikum07/src/ListDir.cpp
@@ -24,20 +24,50 @@ void ListDir::printResult() {
         char *path = files->d_name;
         stat(path, &buf);
         // Ueberspringe Datei, wenn -a nicht angegeben ist und Datei versteckt
-        if(!showAll && path[0] == '.'){
+        if (!showAll && path[0] == '.') {
             files = readdir(currentDir);
             continue;
         }
         if (showLongFormat) {
-            if(showOwner){
-                printf("%-5i ", buf.st_uid);
+            // Dateityp
+            printf((S_ISDIR(buf.st_mode)) ? "d" : "-");
+            // Rechte
+            printf((buf.st_mode & S_IRUSR) ? "r" : "-");
+            printf((buf.st_mode & S_IWUSR) ? "w" : "-");
+            printf((buf.st_mode & S_IXUSR) ? "x" : "-");
+            printf((buf.st_mode & S_IRGRP) ? "r" : "-");
+            printf((buf.st_mode & S_IWGRP) ? "w" : "-");
+            printf((buf.st_mode & S_IXGRP) ? "x" : "-");
+            printf((buf.st_mode & S_IROTH) ? "r" : "-");
+            printf((buf.st_mode & S_IWOTH) ? "w" : "-");
+            printf((buf.st_mode & S_IXOTH) ? "x" : "-");
+            // Anzahl Hardlinks
+            printf(" %i ", buf.st_nlink);
+            // Eigentuemer
+            if (showOwner) {
+#ifdef __linux__
+                struct passwd *pw = getpwuid(buf.st_uid);
+                printf("%-10s ", pw->pw_name);
+#else
+                printf("%-2i ", buf.st_uid);
+#endif
             }
-            if(showGroup){
-                printf("%-5i ", buf.st_gid);
+            // Eigentuemer Gruppe
+            if (showGroup) {
+#ifdef __linux__
+                struct group *gr = getgrgid(buf.st_gid);
+                printf("%-10s ", gr->gr_name);
+#else
+                printf("%-2i ", buf.st_gid);
+#endif
+
             }
+            // Dateigroeße
             printf("%-10i ", buf.st_size);
-            char* time = strtok(ctime(&buf.st_mtime), "\n");
+            // Aenderungszeit
+            char *time = strtok(ctime(&buf.st_mtime), "\n");
             printf("%s ", time);
+            // Name
             printf("%-10s \n", files->d_name);
         } else {
             printf("%s ", files->d_name);
