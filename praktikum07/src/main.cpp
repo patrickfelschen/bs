94be95820a39f3e39d7cbfbf473158b894f54a64
patrickfelschen <patrick@felschen.de>
2022-01-04 10:44:48 +0100

    pdf

--- praktikum07/src/main.cpp
+++ praktikum07/src/main.cpp
@@ -8,30 +8,24 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-
     ListDir listDir;
-
     int c;
     while ((c = getopt(argc, argv, "aglo")) != -1) {
         switch (c) {
             case 'a':
-                //cout << "-a" << endl;
                 listDir.setShowAll(true);
                 break;
             case 'g':
-                //cout << "-g" << endl;
                 listDir.setShowLongFormat(true);
                 listDir.setShowGroup(true);
                 listDir.setShowOwner(false);
                 break;
             case 'l':
-                //cout << "-l" << endl;
                 listDir.setShowLongFormat(true);
                 listDir.setShowGroup(true);
                 listDir.setShowOwner(true);
                 break;
             case 'o':
-                //cout << "-o" << endl;
                 listDir.setShowLongFormat(true);
                 listDir.setShowGroup(false);
                 break;
@@ -43,17 +37,13 @@ int main(int argc, char **argv) {
                 break;
         }
     }
-
     char *directory;
-
     if (optind < argc) {
         directory = argv[optind];
     } else {
         directory = ".";
     }
-
     listDir.setDir(directory);
     listDir.printResult();
-
     return 0;
 }
